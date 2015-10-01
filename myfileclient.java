992731ef6be1504377e3e852cdd6d99a7945a3d9
ailunwang <eleanor.wang.wcs@gmail.com>
2015-09-30 23:01:41 -0400

    fixed the N and M counting issue

---
+++ myfileclient.java
@@ -0,0 +1,88 @@
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.SocketException;
+
+public class myfileclient {
+	public final static int FILE_SIZE = 52428800; // Max file size. Currently unable to find out how to set this dynamically
+	public static void main (String [] args ) throws IOException {
+		int bytesRead;
+		int current = 0;
+		FileOutputStream fos = null;
+		BufferedOutputStream bos = null;
+		OutputStream os = null;
+		DataOutputStream dos = null;
+		InputStream is = null;
+		DataInputStream dis = null;
+		Socket sock = null;
+		int n, m;
+		if (args.length!=3) {
+			System.out.println("Error: This program requires exactly three command line arguments.");
+		}
+		else {
+			try {
+				//creates stream socket connects to args[0]: IP address and args[1]: port
+				sock = new Socket(args[0], Integer.parseInt(args[1]));
+				System.out.println("Connecting...");
+
+				os = sock.getOutputStream();
+				dos = new DataOutputStream(os);
+
+				//args[2]: files to be sent 
+				dos.writeUTF(args[2]);
+				is = sock.getInputStream();
+				dis = new DataInputStream(is);
+
+				String response = dis.readUTF();
+				n = dis.readInt();
+				m = dis.readInt();
+				System.out.println(response);
+				if (response.equals("ReadyToSend")) {
+					System.out.println("Server handled " + n + " requests, " + m + " requests were successful");
+
+					System.out.println("File " + args[2] + " found");
+
+					dos.writeUTF("ReadyToReceive");
+
+					// receive file
+					byte [] mybytearray  = new byte [FILE_SIZE];
+					is = sock.getInputStream();
+					fos = new FileOutputStream(args[0]);
+					bos = new BufferedOutputStream(fos);
+					bytesRead = is.read(mybytearray,0,mybytearray.length);
+					current = bytesRead;
+
+					do {
+						bytesRead =
+								is.read(mybytearray, current, (mybytearray.length-current));
+						if(bytesRead >= 0) current += bytesRead;
+					} while(bytesRead > -1);
+
+					bos.write(mybytearray, 0 , current);
+					bos.flush();
+					System.out.println("Download Completed");
+
+				}
+				else if (response.equals("FileNotFound")) {
+					System.out.println("File " + args[0] + " not found");
+				}
+
+
+
+			}
+			catch (SocketException e) {
+				System.out.println("Connection Not Made");
+			}   	
+			finally {
+				if (fos != null) fos.close();
+				if (bos != null) bos.close();
+				if (sock != null) sock.close();
+			}
+		}
+	}
+}
